b66d11de7d5e2fb2df417a8f3adcf7ef377fde4f
benf999 <benf30.2013@gmail.com>
2020-06-16 16:41:20 +0100

    Fixed the Netlify Identity Widget

--- pages/index.vue
+++ pages/index.vue
@@ -32,15 +32,6 @@
 import Logo from '~/components/Logo.vue'
 
 export default {
-  head() {
-    return {
-      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
-    };
-  },
-};
-
-
-export default {
   components: {
     Logo
   },
